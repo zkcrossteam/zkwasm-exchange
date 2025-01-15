377c8a974f803d6d35cd837fb7545a47ad0cd363
cuiweixie <cuiweixie@gmail.com>
2025-01-16 06:26:09 +0800

    feat: add test check

--- ts/src/test.ts
+++ ts/src/test.ts
@@ -2,9 +2,23 @@
 //import initHostBind, * as hostbind from "./wasmbind/hostbind.js";
 import { Player} from "./api.js";
 import {get_server_admin_key} from "zkwasm-ts-server/src/config.js";
+import {hasSubscribers} from "node:diagnostics_channel";
 let account = "12345";
 let player = new Player(get_server_admin_key(), "http://localhost:3000");
 let playerB = new Player(account, "http://localhost:3000");
+
+const fee = 3n;
+
+async function orderCheck(player:Player, f: ()=> void, check:(before: any, after: any) => boolean) {
+    let before = await player.getState();
+    await f();
+    let after = await player.getState();
+    if(!check(before, after)) {
+        console.log("orderCheck failed");
+        throw new Error("orderCheck failed");
+    }
+}
+
 async function main() {
   //let towerId = 10038n + y;
   let state = await player.getState();
@@ -48,12 +62,68 @@ async function main() {
 
   let name = "limit order buy and market order sell test, b token amount:";
   console.log(name, "add limit order");
-  state = await player.addLimitOrder(1n, 1n, BigInt(1e9), 100n);
-  console.log(state);
+  let f = async () => {
+    await player.addLimitOrder(1n, 1n, BigInt(1e9), 100n);
+  }
+  await orderCheck(player, f, (before, after): boolean => {
+    let tokenIdx = 0;
+    // console.log("before", before);
+    // console.log("after", after);
+    // console.log("before positon", before.player.data.positions[tokenIdx]);
+    // console.log("after positon", after.player.data.positions[tokenIdx]);
+    if(("order_id_counter" in before.state?before.state["order_id_counter"]:0) + 1 != ("order_id_counter" in after.state?after.state["order_id_counter"]:0)) {
+      return false;
+    }
+    // @ts-ignore
+    if (after.player.data.positions[tokenIdx].lock_balance - before.player.data.positions[tokenIdx].lock_balance != 100n + fee) {
+        return false;
+    }
+    // @ts-ignore
+    if (before.player.data.positions[tokenIdx].balance - after.player.data.positions[tokenIdx].balance != 100n + fee) {
+      return false;
+    }
+    return true
+  });
 
   console.log(name, "add market order");
-  state = await playerB.addMarketOrder(1n, 0n, 100n, 0n);
-  console.log(state);
+  f = async () => {
+    await playerB.addMarketOrder(1n, 0n, 100n, 0n);
+  };
+
+  await orderCheck(playerB, f, (before, after): boolean => {
+    // console.log("before", before);
+    // console.log("after", after);
+    // console.log("before positon", before.player.data.positions[tokenIdx]);
+    // console.log("after positon", after.player.data.positions[tokenIdx]);
+    if(("order_id_counter" in before.state?before.state["order_id_counter"]:0) + 1 != ("order_id_counter" in after.state?after.state["order_id_counter"]:0)) {
+      console.log("order_id_counter", before.state["order_id_counter"], after.state["order_id_counter"]);
+      return false;
+    }
+    let tokenIdx = 0;
+    // @ts-ignore
+    if (after.player.data.positions[tokenIdx].lock_balance - before.player.data.positions[tokenIdx].lock_balance != fee) {
+      console.log("fee lock_balance", after.player.data.positions[tokenIdx].lock_balance, before.player.data.positions[tokenIdx].lock_balance);
+      return false;
+    }
+    // @ts-ignore
+    if (before.player.data.positions[tokenIdx].balance - after.player.data.positions[tokenIdx].balance != fee) {
+      console.log("fee balance", before.player.data.positions[tokenIdx].balance, after.player.data.positions[tokenIdx].balance);
+      return false;
+    }
+
+    tokenIdx = 1;
+    // @ts-ignore
+    if (after.player.data.positions[tokenIdx].lock_balance - before.player.data.positions[tokenIdx].lock_balance != 100n) {
+      console.log("lock_balance", after.player.data.positions[tokenIdx].lock_balance, before.player.data.positions[tokenIdx].lock_balance);
+      return false;
+    }
+    // @ts-ignore
+    if (before.player.data.positions[tokenIdx].balance - after.player.data.positions[tokenIdx].balance != 100n) {
+      console.log("balance", before.player.data.positions[tokenIdx].balance, after.player.data.positions[tokenIdx].balance);
+      return false;
+    }
+    return true
+  });
 
   console.log(JSON.stringify(state, null, 2));
 
@@ -63,103 +133,103 @@ async function main() {
   //console.log(state);
   console.log(JSON.stringify(state, null, 2));
 
-  name = "limit order buy and market order sell test, a token amount: ";
-  console.log(name, "add limit order, buy");
-  state = await player.addLimitOrder(1n, 1n, BigInt(1e9), 100n);
-  console.log(state);
-
-  console.log(name, "add market order, sell");
-  state = await playerB.addMarketOrder(1n, 0n, 0n, 100n);
-  console.log(state);
-
+  // name = "limit order buy and market order sell test, a token amount: ";
+  // console.log(name, "add limit order, buy");
+  // state = await player.addLimitOrder(1n, 1n, BigInt(1e9), 100n);
+  // console.log(state);
+  //
+  // console.log(name, "add market order, sell");
+  // state = await playerB.addMarketOrder(1n, 0n, 0n, 100n);
+  // console.log(state);
+  //
+  // // console.log(JSON.stringify(state, null, 2));
+  //
+  // console.log(name, "add trade");
+  // // @ts-ignore
+  // state = await player.addTrace(BigInt(state.state.order_id_counter - 1), BigInt(state.state.order_id_counter), 100n, 100n);
+  // //console.log(state);
   // console.log(JSON.stringify(state, null, 2));
-
-  console.log(name, "add trade");
-  // @ts-ignore
-  state = await player.addTrace(BigInt(state.state.order_id_counter - 1), BigInt(state.state.order_id_counter), 100n, 100n);
-  //console.log(state);
-  console.log(JSON.stringify(state, null, 2));
-
-  name = "limit order sell and market order buy test, market order a token amount:";
-  console.log(name, "add limit order, sell");
-  state = await player.addLimitOrder(1n, 0n, BigInt(1e9), 100n);
-  console.log(state);
-
-  console.log(name, "add market order, buy");
-  state = await playerB.addMarketOrder(1n, 1n, 0n, 100n);
-  console.log(state);
-
-  console.log(name, "add trade");
-  // @ts-ignore
-  state = await player.addTrace(BigInt(state.state.order_id_counter), BigInt(state.state.order_id_counter - 1), 100n, 100n);
-  //console.log(state);
-  console.log(JSON.stringify(state, null, 2));
-
-  name = "limit order sell and market order buy test, market order b token amount:";
-  console.log(name, "add limit order, sell");
-  state = await player.addLimitOrder(1n, 0n, BigInt(1e9), 100n);
-  console.log(state);
-
-  console.log(name, "add market order, buy");
-  state = await playerB.addMarketOrder(1n, 1n, 100n, 0n);
-  console.log(state);
-
-  console.log(name, "add trade");
-  // @ts-ignore
-  state = await player.addTrace(BigInt(state.state.order_id_counter), BigInt(state.state.order_id_counter - 1), 100n, 100n);
-  //console.log(state);
-  console.log(JSON.stringify(state, null, 2));
-
-  console.log("limit order and limit order test");
-
-  console.log("limit order and limit order test, add limit order");
-  state = await player.addLimitOrder(1n, 1n, BigInt(2*1e9), 100n);
-  console.log(state);
-
-  console.log("limit order and limit order test, add limit order");
-  state = await playerB.addLimitOrder(1n, 0n, BigInt(1e9), 100n);
-  console.log(state);
-
-  console.log(JSON.stringify(state, null, 2));
-
-  console.log("limit order and limit order test, add trade");
-  // @ts-ignore
-  state = await player.addTrace(BigInt(state.state.order_id_counter - 1), BigInt(state.state.order_id_counter), 100n, 100n);
-  console.log(state);
-
-  state = await player.getState();
-  console.log(JSON.stringify(state.player, null, 2));
-  state = await playerB.getState();
-  console.log(JSON.stringify(state.player, null, 2));
-  console.log("transfer 1 tokens 0 to the playerB");
-  state = await player.transfer("0c839e6ff76723263a6261ce07813303fd2895a07c67369f657fcf6af320292b", 0n, 1n);
-  console.log(JSON.stringify(state.player, null, 2));
-
-  state = await playerB.getState();
-  console.log(JSON.stringify(state.player, null, 2));
-  console.log("withdraw 1 tokens 0 from the playerB");
-  state = await playerB.withdraw(0n, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", 1n);
-  console.log(JSON.stringify(state.player, null, 2));
-
-  console.log("add limit order");
-  state = await player.addLimitOrder(1n, 1n, 1n, 1n);
-
-  console.log("cancel order");
-  state = await player.cancelOrder(BigInt(state.state.order_id_counter));
-  console.log(JSON.stringify(state.state.orders[state.state.orders.length-1], null, 2));
-
-  console.log("add token 2");
-  state = await player.addToken(2n, "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
-  console.log(state);
-  console.log("add token market");
-  state = await player.addMarket(0n, 2n, 100n);
-  console.log(state);
-
-  console.log("close market");
-  state = await player.closeMarket(BigInt(state.state.market_id_counter));
-  console.log(state);
-
-  console.log(JSON.stringify(state.state, null, 2));
+  //
+  // name = "limit order sell and market order buy test, market order a token amount:";
+  // console.log(name, "add limit order, sell");
+  // state = await player.addLimitOrder(1n, 0n, BigInt(1e9), 100n);
+  // console.log(state);
+  //
+  // console.log(name, "add market order, buy");
+  // state = await playerB.addMarketOrder(1n, 1n, 0n, 100n);
+  // console.log(state);
+  //
+  // console.log(name, "add trade");
+  // // @ts-ignore
+  // state = await player.addTrace(BigInt(state.state.order_id_counter), BigInt(state.state.order_id_counter - 1), 100n, 100n);
+  // //console.log(state);
+  // console.log(JSON.stringify(state, null, 2));
+  //
+  // name = "limit order sell and market order buy test, market order b token amount:";
+  // console.log(name, "add limit order, sell");
+  // state = await player.addLimitOrder(1n, 0n, BigInt(1e9), 100n);
+  // console.log(state);
+  //
+  // console.log(name, "add market order, buy");
+  // state = await playerB.addMarketOrder(1n, 1n, 100n, 0n);
+  // console.log(state);
+  //
+  // console.log(name, "add trade");
+  // // @ts-ignore
+  // state = await player.addTrace(BigInt(state.state.order_id_counter), BigInt(state.state.order_id_counter - 1), 100n, 100n);
+  // //console.log(state);
+  // console.log(JSON.stringify(state, null, 2));
+  //
+  // console.log("limit order and limit order test");
+  //
+  // console.log("limit order and limit order test, add limit order");
+  // state = await player.addLimitOrder(1n, 1n, BigInt(2*1e9), 100n);
+  // console.log(state);
+  //
+  // console.log("limit order and limit order test, add limit order");
+  // state = await playerB.addLimitOrder(1n, 0n, BigInt(1e9), 100n);
+  // console.log(state);
+  //
+  // console.log(JSON.stringify(state, null, 2));
+  //
+  // console.log("limit order and limit order test, add trade");
+  // // @ts-ignore
+  // state = await player.addTrace(BigInt(state.state.order_id_counter - 1), BigInt(state.state.order_id_counter), 100n, 100n);
+  // console.log(state);
+  //
+  // state = await player.getState();
+  // console.log(JSON.stringify(state.player, null, 2));
+  // state = await playerB.getState();
+  // console.log(JSON.stringify(state.player, null, 2));
+  // console.log("transfer 1 tokens 0 to the playerB");
+  // state = await player.transfer("0c839e6ff76723263a6261ce07813303fd2895a07c67369f657fcf6af320292b", 0n, 1n);
+  // console.log(JSON.stringify(state.player, null, 2));
+  //
+  // state = await playerB.getState();
+  // console.log(JSON.stringify(state.player, null, 2));
+  // console.log("withdraw 1 tokens 0 from the playerB");
+  // state = await playerB.withdraw(0n, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", 1n);
+  // console.log(JSON.stringify(state.player, null, 2));
+  //
+  // console.log("add limit order");
+  // state = await player.addLimitOrder(1n, 1n, 1n, 1n);
+  //
+  // console.log("cancel order");
+  // state = await player.cancelOrder(BigInt(state.state.order_id_counter));
+  // console.log(JSON.stringify(state.state.orders[state.state.orders.length-1], null, 2));
+  //
+  // console.log("add token 2");
+  // state = await player.addToken(2n, "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
+  // console.log(state);
+  // console.log("add token market");
+  // state = await player.addMarket(0n, 2n, 100n);
+  // console.log(state);
+  //
+  // console.log("close market");
+  // state = await player.closeMarket(BigInt(state.state.market_id_counter));
+  // console.log(state);
+  //
+  // console.log(JSON.stringify(state.state, null, 2));
 }
 
 main();
